cced697f51fc300078ff2db5fda250ce66fcec6c
chenaoh <cdhenao@gmail.com>
2017-11-12 15:45:53 -0500

    Actualizar y Eliminar Registros

--- app/src/main/java/co/quindio/sena/tutorialwebservice/fragments/ConsultaUsuarioUrlFragment.java
+++ app/src/main/java/co/quindio/sena/tutorialwebservice/fragments/ConsultaUsuarioUrlFragment.java
@@ -2,34 +2,60 @@ package co.quindio.sena.tutorialwebservice.fragments;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import co.quindio.sena.tutorialwebservice.R;
 import co.quindio.sena.tutorialwebservice.entidades.Usuario;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -52,15 +78,30 @@ public class ConsultaUsuarioUrlFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     EditText txtDocumento;
-    TextView etiNombre;
-    TextView etiProfesion;
+    EditText etiNombre;//SE MODIFICA
+    EditText etiProfesion;//SE MODIFICA
     ProgressDialog pDialog;
-    Button btnConsultar;
+    ImageButton btnConsultar;//SE MODIFICA
     ImageView campoImagen;
+    Button btnActualizar, btnEliminar;//SE MODIFICA
+
+    //SE MODIFICA
+    private final int MIS_PERMISOS = 100;
+    private static final int COD_SELECCIONA = 10;
+    private static final int COD_FOTO = 20;
+
+    private static final String CARPETA_PRINCIPAL = "misImagenesApp/";//directorio principal
+    private static final String CARPETA_IMAGEN = "imagenes";//carpeta donde se guardan las fotos
+    private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;//ruta carpeta de directorios
+    private String path;//almacena la ruta de la imagen
+    File fileImagen;
+    Bitmap bitmap;
+    //
 
 
     RequestQueue request;
     JsonObjectRequest jsonObjectRequest;
+    StringRequest stringRequest;//SE MODIFICA
 
     public ConsultaUsuarioUrlFragment() {
         // Required empty public constructor
@@ -99,10 +140,13 @@ public class ConsultaUsuarioUrlFragment extends Fragment {
         View vista= inflater.inflate(R.layout.fragment_consulta_usuario_url, container, false);
 
         txtDocumento= (EditText) vista.findViewById(R.id.campoDocumento);
-        etiNombre= (TextView) vista.findViewById(R.id.txtNombre);
-        etiProfesion= (TextView) vista.findViewById(R.id.txtProfesion);
-        btnConsultar= (Button) vista.findViewById(R.id.btnConsultarUsuario);
+        etiNombre= (EditText) vista.findViewById(R.id.txtNombre);
+        etiProfesion= (EditText) vista.findViewById(R.id.txtProfesion);
+        btnConsultar= (ImageButton) vista.findViewById(R.id.btnConsultarUsuario);//SE MODIFICA
         campoImagen= (ImageView) vista.findViewById(R.id.imagenId);
+        btnActualizar=(Button) vista.findViewById(R.id.btnActualizar);//SE MODIFICA
+        btnEliminar=(Button) vista.findViewById(R.id.btnEliminar);//SE MODIFICA
+
 
         request= Volley.newRequestQueue(getContext());
 
@@ -113,8 +157,44 @@ public class ConsultaUsuarioUrlFragment extends Fragment {
             }
         });
 
+        //SE MODIFICA
+        ////////////////////////////////////////////////////////////
+
+        //Permisos
+        if(solicitaPermisosVersionesSuperiores()){
+            campoImagen.setEnabled(true);
+        }else{
+            campoImagen.setEnabled(false);
+        }
+
+        //evento imagen
+        campoImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarDialogOpciones();
+            }
+        });
+
+        //eventos botones
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webServiceActualizar();
+            }
+        });
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webServiceEliminar();
+            }
+        });
+
+        ///////////////////////////////////////////////////////////
+
         return vista;
     }
+
 
     private void cargarWebService() {
         pDialog=new ProgressDialog(getContext());
@@ -142,8 +222,8 @@ public class ConsultaUsuarioUrlFragment extends Fragment {
                     e.printStackTrace();
                 }
 
-                etiNombre.setText("Nombre :"+miUsuario.getNombre());
-                etiProfesion.setText("Profesion :"+miUsuario.getProfesion());
+                etiNombre.setText(miUsuario.getNombre());//SE MODIFICA
+                etiProfesion.setText(miUsuario.getProfesion());//SE MODIFICA
 
                 String urlImagen="http://192.168.1.55/ejemploBDRemota/"+miUsuario.getRutaImagen();
                 Toast.makeText(getContext(), "url "+urlImagen, Toast.LENGTH_LONG).show();
@@ -169,6 +249,7 @@ public class ConsultaUsuarioUrlFragment extends Fragment {
         ImageRequest imageRequest=new ImageRequest(urlImagen, new Response.Listener<Bitmap>() {
             @Override
             public void onResponse(Bitmap response) {
+                bitmap=response;//SE MODIFICA
                 campoImagen.setImageBitmap(response);
             }
         }, 0, 0, ImageView.ScaleType.CENTER, null, new Response.ErrorListener() {
@@ -179,6 +260,303 @@ public class ConsultaUsuarioUrlFragment extends Fragment {
         });
         request.add(imageRequest);
     }
+
+    //SE MODIFICA
+    //Llamado WebServices, permisos y logica imagen
+    ////////////////
+
+
+    private void webServiceActualizar() {
+        pDialog=new ProgressDialog(getContext());
+        pDialog.setMessage("Cargando...");
+        pDialog.show();
+
+        String url="http://192.168.1.55/ejemploBDRemota/wsJSONUpdateMovil.php?";
+
+        stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                pDialog.hide();
+
+                if (response.trim().equalsIgnoreCase("actualiza")){
+                   // etiNombre.setText("");
+                  //  txtDocumento.setText("");
+                 //   etiProfesion.setText("");
+                    Toast.makeText(getContext(),"Se ha Actualizado con exito",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getContext(),"No se ha Actualizado ",Toast.LENGTH_SHORT).show();
+                    Log.i("RESPUESTA: ",""+response);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(),"No se ha podido conectar",Toast.LENGTH_SHORT).show();
+                pDialog.hide();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                String documento=txtDocumento.getText().toString();
+                String nombre=etiNombre.getText().toString();
+                String profesion=etiProfesion.getText().toString();
+
+                String imagen=convertirImgString(bitmap);
+
+                Map<String,String> parametros=new HashMap<>();
+                parametros.put("documento",documento);
+                parametros.put("nombre",nombre);
+                parametros.put("profesion",profesion);
+                parametros.put("imagen",imagen);
+
+                return parametros;
+            }
+        };
+        request.add(stringRequest);
+
+    }
+
+    private String convertirImgString(Bitmap bitmap) {
+
+        ByteArrayOutputStream array=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,array);
+        byte[] imagenByte=array.toByteArray();
+        String imagenString= Base64.encodeToString(imagenByte,Base64.DEFAULT);
+
+        return imagenString;
+    }
+
+    private void webServiceEliminar() {
+        pDialog=new ProgressDialog(getContext());
+        pDialog.setMessage("Cargando...");
+        pDialog.show();
+
+        String url="http://192.168.1.55/ejemploBDRemota/wsJSONDeleteMovil.php?documento="+txtDocumento.getText().toString();
+
+        stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                pDialog.hide();
+
+                if (response.trim().equalsIgnoreCase("elimina")){
+                    etiNombre.setText("");
+                    txtDocumento.setText("");
+                    etiProfesion.setText("");
+                    campoImagen.setImageResource(R.drawable.img_base);
+                    Toast.makeText(getContext(),"Se ha Eliminado con exito",Toast.LENGTH_SHORT).show();
+                }else{
+                    if (response.trim().equalsIgnoreCase("noExiste")){
+                        Toast.makeText(getContext(),"No se encuentra la persona ",Toast.LENGTH_SHORT).show();
+                        Log.i("RESPUESTA: ",""+response);
+                    }else{
+                        Toast.makeText(getContext(),"No se ha Eliminado ",Toast.LENGTH_SHORT).show();
+                        Log.i("RESPUESTA: ",""+response);
+                    }
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(),"No se ha podido conectar",Toast.LENGTH_SHORT).show();
+                pDialog.hide();
+            }
+        });
+        request.add(stringRequest);
+    }
+
+    private void mostrarDialogOpciones() {
+        final CharSequence[] opciones={"Tomar Foto","Elegir de Galeria","Cancelar"};
+        final AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+        builder.setTitle("Elige una Opción");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("Tomar Foto")){
+                    abriCamara();
+                }else{
+                    if (opciones[i].equals("Elegir de Galeria")){
+                        Intent intent=new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent,"Seleccione"),COD_SELECCIONA);
+                    }else{
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void abriCamara() {
+        File miFile=new File(Environment.getExternalStorageDirectory(),DIRECTORIO_IMAGEN);
+        boolean isCreada=miFile.exists();
+
+        if(isCreada==false){
+            isCreada=miFile.mkdirs();
+        }
+
+        if(isCreada==true){
+            Long consecutivo= System.currentTimeMillis()/1000;
+            String nombre=consecutivo.toString()+".jpg";
+
+            path=Environment.getExternalStorageDirectory()+File.separator+DIRECTORIO_IMAGEN
+                    +File.separator+nombre;//indicamos la ruta de almacenamiento
+
+            fileImagen=new File(path);
+
+            Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(fileImagen));
+
+            ////
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N)
+            {
+                String authorities=getContext().getPackageName()+".provider";
+                Uri imageUri= FileProvider.getUriForFile(getContext(),authorities,fileImagen);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            }else
+            {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(fileImagen));
+            }
+            startActivityForResult(intent,COD_FOTO);
+
+            ////
+
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case COD_SELECCIONA:
+                Uri miPath=data.getData();
+                campoImagen.setImageURI(miPath);
+
+                try {
+                    bitmap=MediaStore.Images.Media.getBitmap(getContext().getContentResolver(),miPath);
+                    campoImagen.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            case COD_FOTO:
+                MediaScannerConnection.scanFile(getContext(), new String[]{path}, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String path, Uri uri) {
+                                Log.i("Path",""+path);
+                            }
+                        });
+
+                bitmap= BitmapFactory.decodeFile(path);
+                campoImagen.setImageBitmap(bitmap);
+
+                break;
+        }
+        bitmap=redimensionarImagen(bitmap,600,800);
+    }
+
+    private Bitmap redimensionarImagen(Bitmap bitmap, float anchoNuevo, float altoNuevo) {
+
+        int ancho=bitmap.getWidth();
+        int alto=bitmap.getHeight();
+
+        if(ancho>anchoNuevo || alto>altoNuevo){
+            float escalaAncho=anchoNuevo/ancho;
+            float escalaAlto= altoNuevo/alto;
+
+            Matrix matrix=new Matrix();
+            matrix.postScale(escalaAncho,escalaAlto);
+
+            return Bitmap.createBitmap(bitmap,0,0,ancho,alto,matrix,false);
+
+        }else{
+            return bitmap;
+        }
+
+    }
+
+    //PERMISOS
+    private boolean solicitaPermisosVersionesSuperiores() {
+        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.M){//validamos si estamos en android menor a 6 para no buscar los permisos
+            return true;
+        }
+
+        //validamos si los permisos ya fueron aceptados
+        if((getContext().checkSelfPermission(WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED)&&getContext().checkSelfPermission(CAMERA)==PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+
+
+        if ((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)||(shouldShowRequestPermissionRationale(CAMERA)))){
+            cargarDialogoRecomendacion();
+        }else{
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, MIS_PERMISOS);
+        }
+
+        return false;//implementamos el que procesa el evento dependiendo de lo que se defina aqui
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==MIS_PERMISOS){
+            if(grantResults.length==2 && grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED){//el dos representa los 2 permisos
+                Toast.makeText(getContext(),"Permisos aceptados",Toast.LENGTH_SHORT);
+                campoImagen.setEnabled(true);//se vincula el evento a la imagen
+            }
+        }else{
+            solicitarPermisosManual();
+        }
+    }
+
+
+    private void solicitarPermisosManual() {
+        final CharSequence[] opciones={"si","no"};
+        final AlertDialog.Builder alertOpciones=new AlertDialog.Builder(getContext());//estamos en fragment
+        alertOpciones.setTitle("¿Desea configurar los permisos de forma manual?");
+        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("si")){
+                    Intent intent=new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri=Uri.fromParts("package",getContext().getPackageName(),null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getContext(),"Los permisos no fueron aceptados",Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        alertOpciones.show();
+    }
+
+
+    private void cargarDialogoRecomendacion() {
+        AlertDialog.Builder dialogo=new AlertDialog.Builder(getContext());
+        dialogo.setTitle("Permisos Desactivados");
+        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
+
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,CAMERA},100);
+            }
+        });
+        dialogo.show();
+    }
+
+    ///////////////
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
